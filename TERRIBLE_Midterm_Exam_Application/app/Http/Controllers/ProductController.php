e1782cd9797d7477c6bd5f7db00bcec2012715ce
AngeloGerardTerrible <144972042+AngeloGerardTerrible@users.noreply.github.com>
2025-02-24 14:28:58 +0000

    Update: Model and seeder

--- TERRIBLE_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
+++ TERRIBLE_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Anime;
+use App\Models\Anime; // Import the Anime model
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $animes = Anime::all();
+        $animes = Anime::all(); // Fetch all anime from the database
+
         return view('welcome', ['products' => $animes]);
     }
 }
