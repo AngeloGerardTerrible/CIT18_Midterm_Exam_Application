4f461beb08019c29f35304b9564e3f5341f5d3d6
AngeloGerardTerrible <144972042+AngeloGerardTerrible@users.noreply.github.com>
2025-02-24 13:53:47 +0000

    Update: Blade Template

--- TERRIBLE_Midterm_Exam_Application/app/Models/Anime.php
+++ TERRIBLE_Midterm_Exam_Application/app/Models/Anime.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Anime extends Model
 {
-    //
-    
+    use HasFactory;
+
+    protected $fillable = ['title', 'genre', 'episodes'];
 }
--- TERRIBLE_Midterm_Exam_Application/routes/welcome.blade.php
+++ TERRIBLE_Midterm_Exam_Application/routes/welcome.blade.php
@@ -11,9 +11,6 @@
                 <h2>{{ $anime->title }}</h2>
                 <p>Genre: {{ $anime->genre }}</p>
                 <p>Episodes: {{ $anime->episodes }}</p>
-                @if ($anime->image)
-                    <img src="{{ asset('images/' . $anime->image) }}" alt="{{ $anime->title }}" style="max-width: 200px;">
-                @endif
             </li>
         @endforeach
     </ul>
